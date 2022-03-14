6ac68b516330ef86dead63fd67c34621e77cca69
Dimitris Kefalos <dkefalos@gmail.com>
2022-03-14 20:06:59 +0200

    Initial Commit

---
+++ ChessKnight/ChessKnight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessKnight
+//
+//  Created by Dimitris Kefalos on 14/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
